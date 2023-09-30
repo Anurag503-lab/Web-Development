fee38fd98fe13772c781bad71246ea4a332496da
Anurag503-lab <75729983+Anurag503-lab@users.noreply.github.com>
2023-09-30 17:56:41 +0000

    conversion of datatypes

---
+++ 01_BASICS/03_conversion.js
@@ -0,0 +1,15 @@
+// let score = 33
+//console.log(typeof score)
+//console.log(typeof score)
+//"33" -> 33
+//"33abc"-> NaN
+//true -> 1; false ->
+ //let isloogedIn= "ansn"
+ //let booleanIsLoggedIn = Boolean(isloogedIn)
+ //console.log(booleanIsLoggedIn)
+ // 1-> true -> false 
+ //"" -> false
+ // "hitesh" - > true
+ let someNumber = 33
+ let stringNumber = String(someNumber);
+ console.log( stringNumber)
